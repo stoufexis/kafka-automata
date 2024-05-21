f35589ef68ddc91c63abcbcbad38d08d061d5ee7
Stefanos Toufexis <stefetoufe@gmail.com>
2024-05-22 01:10:15 +0300

    Less object-orientation

--- examples/voting/src/main/scala/com/stoufexis/fsm/examples/voting/fsm/VoteFSM.scala
+++ examples/voting/src/main/scala/com/stoufexis/fsm/examples/voting/fsm/VoteFSM.scala
@@ -8,77 +8,73 @@ import com.stoufexis.fsm.lib.fsm.FSM
 import fs2._
 import io.chrisdavenport.fuuid.FUUIDGen
 
-/** Input is expected to be keyed by itemId in kafka. If it's not, we reject it.
-  */
-class VoteFSM[F[_]: Monad: FUUIDGen] extends FSM.Unbatched[F, ItemId, Votes, VoteCommand, Output] {
+object VoteFSM {
 
-  def reject(
-    state:  Option[Votes],
-    cmd:    VoteCommand,
-    reason: String
-  ): F[(Option[Votes], Chunk[Output])] =
-    VoteEvent.commandRejected(cmd, reason) map { event =>
-      (state, Chunk.singleton(Output.Event(event)))
+  def apply[F[_]: Monad: FUUIDGen]: FSM[F, ItemId, Votes, VoteCommand, Output] = {
+    def reject(
+      state:  Option[Votes],
+      cmd:    VoteCommand,
+      reason: String
+    ): F[(Option[Votes], Chunk[Output])] =
+      VoteEvent.commandRejected(cmd, reason) map { event =>
+        (state, Chunk.singleton(Output.Event(event)))
+      }
+
+    def execute(state: Option[Votes], cmd: VoteCommand): F[(Option[Votes], Chunk[Output])] =
+      for {
+        event: VoteEvent <-
+          VoteEvent.commandExecuted(cmd)
+
+        update: Option[VoteStateUpdate] <-
+          state.traverse { votes =>
+            VoteStateUpdate.make(cmd.correlationId, cmd.itemId, votes.total)
+          }
+
+      } yield (state, Output.both(event, update))
+
+    def fold(item: ItemId): (Option[Votes], VoteCommand) => F[(Option[Votes], Chunk[Output])] = {
+      // Input validation: check if someone sent us badly keyed data.
+      // This could be avoided if VoteCommand did not also contain itemId, but we opt for this currently.
+      case (state, cmd: VoteCommand) if cmd.itemId != item =>
+        reject(state, cmd, s"Command ${cmd.id} was keyed incorrectly: ${cmd.itemId} != ${item}")
+
+      // There already exists state for this item, so vote has already started
+      case (st @ Some(_), cmd: VoteCommand.VoteStart) =>
+        reject(st, cmd, s"Voting has already begun for $item")
+
+      // End the vote for `item`
+      case (Some(_), cmd: VoteCommand.VoteEnd) =>
+        execute(None, cmd)
+
+      // Start the vote for `item`
+      case (None, cmd: VoteCommand.VoteStart) =>
+        execute(Some(Votes.empty), cmd)
+
+      // There is no open vote, so we cannot execute anything other than VoteStart
+      case (None, cmd) =>
+        reject(None, cmd, s"No open vote for $item")
+
+      // For the following cases note that Votes already makes sure that there cannot be
+      // double upvotes/downvotes, but we explicitly handle it here to output rejection events
+
+      // Users cannot upvote the same item twice
+      case (Some(st), cmd: VoteCommand.Upvote) if st upvotedBy cmd.userId =>
+        reject(Some(st), cmd, s"User ${cmd.userId} has already upvoted $item")
+
+      // User upvotes `item`
+      case (Some(st), cmd: VoteCommand.Upvote) =>
+        execute(Some(st upvote cmd.userId), cmd)
+
+      // Users cannot downvote the same item twice
+      case (Some(st), cmd: VoteCommand.Downvote) if st downvotedBy cmd.userId =>
+        reject(Some(st), cmd, s"User ${cmd.userId} has already downvoted $item")
+
+      // User downvotes `item`
+      case (Some(st), cmd: VoteCommand.Downvote) =>
+        execute(Some(st downvote cmd.userId), cmd)
+
     }
 
-  def execute(state: Option[Votes], cmd: VoteCommand): F[(Option[Votes], Chunk[Output])] =
-    for {
-      event: VoteEvent <-
-        VoteEvent.commandExecuted(cmd)
-
-      update: Option[VoteStateUpdate] <-
-        state.traverse { votes =>
-          VoteStateUpdate.make(cmd.correlationId, cmd.itemId, votes.total)
-        }
-
-    } yield (state, Output.both(event, update))
-
-  // Aliased to make it easier to read
-  type Fold = (Option[Votes], VoteCommand) => F[(Option[Votes], Chunk[Output])]
-
-  /** @param item
-    *   We will be sent commands for this item.
-    */
-  override def singleton(item: ItemId): Fold = {
-    // Input validation: check if someone sent us badly keyed data.
-    // This could be avoided if VoteCommand did not also contain itemId, but we opt for this currently.
-    case (state, cmd: VoteCommand) if cmd.itemId != item =>
-      reject(state, cmd, s"Command ${cmd.id} was keyed incorrectly: ${cmd.itemId} != ${item}")
-
-    // There already exists state for this item, so vote has already started
-    case (st @ Some(_), cmd: VoteCommand.VoteStart) =>
-      reject(st, cmd, s"Voting has already begun for $item")
-
-    // Start the vote for `item`
-    case (None, cmd: VoteCommand.VoteStart) =>
-      execute(Some(Votes.empty), cmd)
-
-    // End the vote for `item`
-    case (Some(_), cmd: VoteCommand.VoteEnd) =>
-      execute(None, cmd)
-
-    // There is no open vote, so we cannot execute these
-    case (None, cmd @ (_: VoteCommand.Downvote | _: VoteCommand.Upvote | _: VoteCommand.VoteEnd)) =>
-      reject(None, cmd, s"No open vote for $item")
-
-    // For the following cases note that Votes already makes sure that there cannot be
-    // double upvotes/downvotes, but we explicitly handle it here to output rejection events
-
-    // Users cannot upvote the same item twice
-    case (Some(st), cmd: VoteCommand.Upvote) if st upvotedBy cmd.userId =>
-      reject(Some(st), cmd, s"User ${cmd.userId} has already upvoted $item")
-
-    // User upvotes `item`
-    case (Some(st), cmd: VoteCommand.Upvote) =>
-      execute(Some(st upvote cmd.userId), cmd)
-
-    // Users cannot downvote the same item twice
-    case (Some(st), cmd: VoteCommand.Downvote) if st downvotedBy cmd.userId =>
-      reject(Some(st), cmd, s"User ${cmd.userId} has already downvoted $item")
-
-    // User downvotes `item`
-    case (Some(st), cmd: VoteCommand.Downvote) =>
-      execute(Some(st downvote cmd.userId), cmd)
-
+    FSM.unbatched(fold)
   }
 }
--- lib/src/main/scala/com/stoufexis/fsm/lib/FSM.scala
+++ lib/src/main/scala/com/stoufexis/fsm/lib/FSM.scala
@@ -9,24 +9,21 @@ trait FSM[F[_], InstanceId, State, In, Out] {
 }
 
 object FSM {
-  abstract class Unbatched[F[_], InstId, State, In, Out](
-    implicit F: Monad[F]
-  ) extends FSM[F, InstId, State, In, Out] {
+  def unbatched[F[_]: Monad, InstanceId, State, In, Out](
+    f: InstanceId => (Option[State], In) => F[(Option[State], Chunk[Out])]
+  ): FSM[F, InstanceId, State, In, Out] = new FSM[F, InstanceId, State, In, Out] {
 
-    def singleton(instance: InstId): (Option[State], In) => F[(Option[State], Chunk[Out])]
-
-    override final def batch(
-      instance: InstId
+    override def batch(
+      instance: InstanceId
     ): (Option[State], Chunk[In]) => F[(Option[State], Chunk[Out])] = {
-
       // TODO: Can be optimised
       def loop(st: Option[State], ins: List[In], out: Chunk[Out]): F[(Option[State], Chunk[Out])] =
         ins match {
           case Nil =>
-            F.pure(st, out)
+            Monad[F].pure(st, out)
 
           case head :: tail =>
-            singleton(instance)(st, head) >>= {
+            f(instance)(st, head) >>= {
               case (nextSt, nextOuts) => loop(nextSt, tail, out ++ nextOuts)
             }
         }
